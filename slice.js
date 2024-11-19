0f70057344a1ec646fcf3103e87cb25e6fc1c7db
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-20 01:34:36 +0600

    updated

--- slice.js
+++ slice.js
@@ -3,14 +3,18 @@ const makeup ="HUDABEAUTY ";
 //const part = makeup.slice(4,10);
 console.log(makeup.slice(4,10));
 
-// split
+// split-sentexce theke array te convert
 const sentence ="jahid is a good and hardworking man";
 console.log(sentence.split(' '));
-
-const frnds = 'jakir,jahid,karim,rahim';
+const modiarray = sentence.split(' ');
+for(let i=0; i<modiarray.length; i++ ){
+    console.log(modiarray[i].toUpperCase());
+}
+/*const frnds = 'jakir,jahid,karim,rahim';
 console.log(frnds.split(','));
 
 const realFrnd = [ 'montu', 'jhontu', 'lulu','niklu'];
 console.log(realFrnd.join());
 console.log(realFrnd.join('-'));
 console.log(realFrnd.join('|'));
+*/
