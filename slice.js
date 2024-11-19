4cf58c063a27d9c9c50b2afba1b823b7b31a98cf
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-20 00:42:12 +0600

    slicing

---
+++ slice.js
@@ -0,0 +1,16 @@
+//slice -  sentence er kichu word baad dewa
+const makeup ="HUDABEAUTY ";
+//const part = makeup.slice(4,10);
+console.log(makeup.slice(4,10));
+
+// split
+const sentence ="jahid is a good and hardworking man";
+console.log(sentence.split(' '));
+
+const frnds = 'jakir,jahid,karim,rahim';
+console.log(frnds.split(','));
+
+const realFrnd = [ 'montu', 'jhontu', 'lulu','niklu'];
+console.log(realFrnd.join());
+console.log(realFrnd.join('-'));
+console.log(realFrnd.join('|'));
