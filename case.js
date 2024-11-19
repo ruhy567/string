3387d3946b63c9f541857f7008053b68b0a96fac
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-19 23:54:02 +0600

    casing

---
+++ case.js
@@ -0,0 +1,34 @@
+const school = 'Ankur school';
+const clg = 'ankur school';
+if (school === clg ){
+    console.log("My school");
+}
+else{
+    console.log('clg');
+}
+
+//upper-lowercase
+const makeup ="HUDA BEAUTY Foundation";
+const lipstick = "huda beauty foundation";
+if(makeup.toLowerCase() === lipstick.toLowerCase()){
+    console.log("kinbo");
+}
+else{
+    console.log("kinbo na");
+}
+
+
+const name = "Jahid Ruhy";
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+//trim-shamne ba piochine space thik korar jonno use hoi
+
+const drink = 'mojo';
+const liqiud = ' mojo ';
+if(drink.trim() === liqiud.trim()){
+    console.log("khabo");
+}
+else{
+    console.log("khabo na");
+}
